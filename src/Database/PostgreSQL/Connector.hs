99a0c4d1398261f2be57631a414b0d15d904a240
Mark Fine <mark.fine@gmail.com>
2015-11-24 14:01:05 -0800

    Add a connection pool.

--- src/Database/PostgreSQL/Connector.hs
+++ src/Database/PostgreSQL/Connector.hs
@@ -15,11 +15,14 @@
 
 module Database.PostgreSQL.Connector where
 
-import Control.Lens
-import Control.Monad.Base
-import Control.Monad.Reader
-import Control.Monad.Trans.Resource
-import Data.Int
+import           Control.Concurrent
+import           Control.Lens
+import           Control.Monad.Base
+import           Control.Monad.Catch
+import           Control.Monad.Reader
+import           Control.Monad.Trans.Resource
+import           Data.ByteString
+import           Data.Int
 import qualified Database.PostgreSQL.Simple as PG
 
 newtype ConnectorT e m a = ConnectorT
@@ -27,7 +30,9 @@ newtype ConnectorT e m a = ConnectorT
   } deriving ( Functor
              , Applicative
              , Monad
+             , MonadCatch
              , MonadIO
+             , MonadMask
              , MonadReader e
              , MonadThrow
              )
@@ -41,53 +46,83 @@ instance MonadTrans (ConnectorT r) where
 instance MonadResource m => MonadResource (ConnectorT r m) where
   liftResourceT = lift . liftResourceT
 
-newtype Conn = Conn
-  { _connConnection :: PG.Connection
+data Conn = Conn
+  { _connDatabaseUrl    :: ByteString
+  , _connConnectionPool :: MVar [PG.Connection]
   }
 
 $(makeClassy ''Conn)
 
 type MonadConnector e m =
   ( MonadIO       m
+  , MonadMask     m
   , MonadResource m
   , MonadReader e m
   , HasConn     e
   )
 
+newConn :: MonadIO m => ByteString -> m Conn
+newConn databaseUrl = do
+  connectionPool <- liftIO $ newMVar []
+  return Conn
+    { _connDatabaseUrl = databaseUrl
+    , _connConnectionPool = connectionPool
+    }
+
 runConnectorT :: HasConn e => e -> ConnectorT e m a -> m a
 runConnectorT e (ConnectorT m) = runReaderT m e
 
+connect :: MonadConnector e m => m PG.Connection
+connect = do
+  connectionPool <- view connConnectionPool
+  databaseUrl    <- view connDatabaseUrl
+  liftIO $ modifyMVar connectionPool $ \connectionPool' ->
+    case connectionPool' of
+      (connection:connections) -> return (connections, connection)
+      [] -> do
+        connection <- PG.connectPostgreSQL databaseUrl
+        return ([], connection)
+
+restore :: MonadConnector e m => PG.Connection -> m ()
+restore connection = do
+  connectionPool <- view connConnectionPool
+  liftIO $ modifyMVar_ connectionPool $ \connectionPool' ->
+    return $ connection:connectionPool'
+
+withConnection :: MonadConnector e m => (PG.Connection -> m a) -> m a
+withConnection = bracket connect restore
+
 withTransaction :: MonadConnector e m => IO a -> m a
-withTransaction action = do
-  connection <- view connConnection
-  liftIO $ PG.withTransaction connection action
+withTransaction action =
+  withConnection $ \connection ->
+    liftIO $ PG.withTransaction connection action
 
 query :: (MonadConnector e m, PG.ToRow q, PG.FromRow r) => PG.Query -> q -> m [r]
-query q params = do
-  connection <- view connConnection
-  liftIO $ PG.query connection q params
+query q params =
+  withConnection $ \connection ->
+    liftIO $ PG.query connection q params
 
 query_ :: (MonadConnector e m, PG.FromRow r) => PG.Query -> m [r]
-query_ q = do
-  connection <- view connConnection
-  liftIO $ PG.query_ connection q
+query_ q =
+  withConnection $ \connection ->
+    liftIO $ PG.query_ connection q
 
 execute :: (MonadConnector e m, PG.ToRow q) => PG.Query -> q -> m Int64
-execute q params = do
-  connection <- view connConnection
-  liftIO $ PG.execute connection q params
+execute q params =
+  withConnection $ \connection ->
+    liftIO $ PG.execute connection q params
 
 executeMany :: (MonadConnector e m, PG.ToRow q) => PG.Query -> [q] -> m Int64
-executeMany q params = do
-  connection <- view connConnection
-  liftIO $ PG.executeMany connection q params
+executeMany q params =
+  withConnection $ \connection ->
+    liftIO $ PG.executeMany connection q params
 
 execute_ :: MonadConnector e m => PG.Query -> m Int64
-execute_ q = do
-  connection <- view connConnection
-  liftIO $ PG.execute_ connection q
+execute_ q =
+  withConnection $ \connection ->
+    liftIO $ PG.execute_ connection q
 
 returning :: (MonadConnector e m, PG.ToRow q, PG.FromRow r) => PG.Query -> [q] -> m [r]
-returning q params = do
-  connection <- view connConnection
-  liftIO $ PG.returning connection q params
+returning q params =
+  withConnection $ \connection ->
+    liftIO $ PG.returning connection q params
