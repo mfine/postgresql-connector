f63fc049e11d9e7403a4ce14151b242317dd05fc
Mark Fine <mark.fine@gmail.com>
2015-11-24 15:13:03 -0800

    Examples.

---
+++ examples/src/Example/Connector.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Example.Connector where
+
+import BasicPrelude
+import Control.Monad.Trans.Resource
+import Database.PostgreSQL.Connector
+import Database.PostgreSQL.Simple ( Only )
+
+basicQuery :: IO ()
+basicQuery = do
+  c <- newConn "postgres://localhost"
+  runResourceT $ runConnectorT c $ do
+    r <- query_ "select 2 + 2"
+    liftIO $ print (r :: [Only Int])
+
